b26e96ba3ff008f4c02da061b2599c0c25a2d0c1
VedantRD <vedant.debadwar@gmail.com>
2023-03-04 19:36:18 +0530

    nav changes

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -7,11 +7,11 @@ const MyNavbar = () => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ms-auto" id="my-navbar">
-          <a href="#home" className='nav-link text ms-2 ms-lg-3'>Home</a>
-          <a href="#about" className='nav-link text ms-2 ms-lg-3'>About</a>
-          <a href="#experience" className='nav-link text ms-2 ms-lg-3'>Experience</a>
-          <a href="#projects" className='nav-link text ms-2 ms-lg-3'>Projects</a>
-          <a href="#contact" className='nav-link text ms-2 ms-lg-3'>Contact</a>
+          <a href="./#home" className='nav-link text ms-2 ms-lg-3'>Home</a>
+          <a href="./#about" className='nav-link text ms-2 ms-lg-3'>About</a>
+          <a href="./#experience" className='nav-link text ms-2 ms-lg-3'>Experience</a>
+          <a href="./#projects" className='nav-link text ms-2 ms-lg-3'>Projects</a>
+          <a href="./#contact" className='nav-link text ms-2 ms-lg-3'>Contact</a>
         </Nav>
       </Navbar.Collapse>
     </Navbar >
